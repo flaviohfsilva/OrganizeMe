b812c451fe303341ab2e6cd86da95ff4420188f2
Gabriela Diniz <gadisa10@outlook.com>
2024-06-06 23:34:07 -0300

    💄 style: tela de biblioteca

--- lib/pages/library/library.dart
+++ lib/pages/library/library.dart
@@ -4,105 +4,136 @@ class Biblioteca extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.all(16.0),
+      body: Column(
         children: [
-          // Header
+          // Container azul com visão
           Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            color: Colors.blue.shade100,
+            width: double.infinity,
+            color: Colors.blue.shade200,
+            padding: EdgeInsets.symmetric(vertical: 40.0), // Aumente o valor do padding aqui
             child: Column(
               children: [
                 Text(
                   'Viaje pelas páginas e explore novos mundos',
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 18.0,
+                    fontSize: 20.0,
                     fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Busque por autor, título ou ISBN',
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+                  padding: EdgeInsets.symmetric(horizontal: 50.0),
+                  child: Container(
+                    margin: EdgeInsets.only(top: 20.0), // Adiciona margem superior
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Busque por autor, título ou ISBN',
+                        hintStyle: TextStyle(
+                          color: Colors.blue.shade200, // Cor do hint text
+                        ),
+                        suffixIcon: Icon(
+                          Icons.search,
+                          color: Colors.blue.shade400, // Cor do ícone
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
                       ),
-                      filled: true,
-                      fillColor: Colors.white,
+                      style: TextStyle(
+                        color: Colors.blue.shade400, // Cor do texto
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
           ),
-          SizedBox(height: 20),
-          // Sugerido para você
-          Text(
-            'Sugerido para você',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            height: 200.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: 120.0,
-                  margin: EdgeInsets.only(right: 10.0),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Center(
-                    child: Image.asset(
-                      'assets/book-hp.jpeg',
-                      fit: BoxFit.cover,
+          // Container livros sugeridos
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: ListView(
+                children: [
+                  // Sugerido para você
+                  Text(
+                    'Sugerido para você',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                );
-              },
-            ),
-          ),
-          SizedBox(height: 20),
-          // Conheça novos títulos
-          Text(
-            'Conheça novos títulos',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            height: 200.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: 120.0,
-                  margin: EdgeInsets.only(right: 10.0),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Center(
-                    child: Image.asset(
-                      'assets/book-hp.jpeg',
-                      fit: BoxFit.cover,
+                  const SizedBox(height: 10),
+                  Container(
+                    height: 200.0,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: 120.0,
+                          margin: const EdgeInsets.only(right: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: Center(
+                            child: Image.asset(
+                              'assets/book-hp.jpeg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                );
-              },
+                  const SizedBox(height: 20),
+                  // Conheça novos títulos
+                  Text(
+                    'Conheça novos títulos',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    height: 200.0,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: 120.0,
+                          margin: const EdgeInsets.only(right: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: Center(
+                            child: Image.asset(
+                              'assets/book-hp.jpeg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
