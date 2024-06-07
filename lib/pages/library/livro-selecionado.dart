cd7f4323280a06189a634b856f7a04df3b28de62
Gabriela Diniz <gadisa10@outlook.com>
2024-06-07 00:23:32 -0300

    🎨 chore: ajuste botões

--- lib/pages/library/livro-selecionado.dart
+++ lib/pages/library/livro-selecionado.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-class LivroSelecionado extends StatelessWidget {
+class LivroSelecionado extends StatefulWidget {
+  @override
+  _LivroSelecionadoState createState() => _LivroSelecionadoState();
+}
+
+class _LivroSelecionadoState extends State<LivroSelecionado> {
+  String _tipoSelecionado = 'Livro'; // Valor padrão
+  String _statusSelecionado = 'Quero Ler'; // Valor padrão
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,18 +22,20 @@ class LivroSelecionado extends StatelessWidget {
           },
         ),
       ),
-      body: ListView(
-        padding: EdgeInsets.all(16.0),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          // Header with book image and details
+          // Container azul com imagem e detalhes do livro
           Container(
+            color: Colors.blue.shade200,
             padding: EdgeInsets.all(20.0),
-            color: Colors.blue.shade100,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image.network(
-                  'https://link-to-harry-potter-book-cover.jpg',
-                  height: 150,
+                Image.asset(
+                  'assets/book-hp.jpeg',
+                  fit: BoxFit.cover,
+                  height: 150.0,
                 ),
                 SizedBox(height: 10),
                 Text(
@@ -33,6 +43,7 @@ class LivroSelecionado extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 20.0,
                     fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
                 SizedBox(height: 5),
@@ -40,7 +51,7 @@ class LivroSelecionado extends StatelessWidget {
                   'J.K. Rowling',
                   style: TextStyle(
                     fontSize: 16.0,
-                    color: Colors.grey,
+                    color: Colors.white,
                   ),
                 ),
                 SizedBox(height: 5),
@@ -48,113 +59,179 @@ class LivroSelecionado extends StatelessWidget {
                   'Rocco',
                   style: TextStyle(
                     fontSize: 16.0,
-                    color: Colors.grey,
+                    color: Colors.white,
                   ),
                 ),
               ],
             ),
           ),
-          SizedBox(height: 20),
-          // Sinopse
-          Text(
-            'Sinopse',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
+          // Container branco com sinopse, status do livro e detalhes
           Container(
-            padding: EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey.shade300),
-              borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Sinopse
+                Text(
+                  'Sinopse',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade300),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu scelerisque purus. Aenean venenatis iaculis rutrum. Donec nisl turpis, hendrerit id ligula non, tincidunt interdum turpis. Nulla nisi nulla, finibus sit amet nisi non, mollis eleifend nisi. Integer pulvinar justo dui...',
+                  ),
+                ),
+                SizedBox(height: 20),
+                // Dados do livro
+                ListTile(
+                  leading: Icon(Icons.book),
+                  title: Text('Dados do livro'),
+                  onTap: () {
+                    // Navegar para a página de dados do livro
+                  },
+                ),
+                SizedBox(height: 20),
+                // Tipo de livro
+                Text(
+                  'Qual o seu tipo de livro?',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _tipoSelecionado = 'Livro';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _tipoSelecionado == 'Livro'
+                            ? Color(0xFFA4C4FF)
+                            : Color(0xFFFEFAED),
+                      ),
+                      child: Text('Livro'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _tipoSelecionado = 'Ebook';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _tipoSelecionado == 'Ebook'
+                            ? Color(0xFFA4C4FF)
+                            : Color(0xFFFEFAED),
+                      ),
+                      child: Text('Ebook'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _tipoSelecionado = 'Áudio-livro';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _tipoSelecionado == 'Áudio-livro'
+                            ? Color(0xFFA4C4FF)
+                            : Color(0xFFFEFAED),
+                      ),
+                      child: Text('Áudio-livro'),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                // Status do livro
+                Text(
+                  'Como está o seu status para esse livro?',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _statusSelecionado = 'Quero Ler';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _statusSelecionado == 'Quero Ler'
+                            ? Color(0xFFA4C4FF)
+                            : Color(0xFFFEFAED),
+                      ),
+                      child: Text('Quero Ler'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _statusSelecionado = 'Lendo';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _statusSelecionado == 'Lendo'
+                            ? Color(0xFFA4C4FF)
+                            : Color(0xFFFEFAED),
+                      ),
+                      child: Text('Lendo'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _statusSelecionado = 'Lido';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _statusSelecionado == 'Lido'
+                            ? Color(0xFFA4C4FF)
+                            : Color(0xFFFEFAED),
+                      ),
+                      child: Text('Lido'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _statusSelecionado = 'Abandonei';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _statusSelecionado == 'Abandonei'
+                            ? Color(0xFFA4C4FF)
+                            : Color(0xFFFEFAED),
+                      ),
+                      child: Text('Abandonei'),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            child: Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu scelerisque purus. Aenean venenatis iaculis rutrum. Donec nisl turpis, hendrerit id ligula non, tincidunt interdum turpis. Nulla nisi nulla, finibus sit amet nisi non, mollis eleifend nisi. Integer pulvinar justo dui...',
-            ),
-          ),
-          SizedBox(height: 10),
-          TextButton(
-            onPressed: () {
-              // Navegar para a página completa da sinopse
-            },
-            child: Text(
-              'Ler mais',
-              style: TextStyle(
-                color: Colors.blue,
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          // Dados do livro
-          ListTile(
-            leading: Icon(Icons.book),
-            title: Text('Dados do livro'),
-            onTap: () {
-              // Navegar para a página de dados do livro
-            },
-          ),
-          SizedBox(height: 20),
-          // Tipo de livro
-          Text(
-            'Qual o seu tipo de livro?',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Chip(
-                label: Text('Livro'),
-                backgroundColor: Colors.blue.shade100,
-              ),
-              Chip(
-                label: Text('Ebook'),
-                backgroundColor: Colors.orange.shade100,
-              ),
-              Chip(
-                label: Text('Áudio-livro'),
-                backgroundColor: Colors.yellow.shade100,
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          // Status do livro
-          Text(
-            'Como está o seu status para esse livro?',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Chip(
-                label: Text('Quero Ler'),
-                backgroundColor: Colors.blue.shade100,
-              ),
-              Chip(
-                label: Text('Lendo'),
-                backgroundColor: Colors.green.shade100,
-              ),
-              Chip(
-                label: Text('Lido'),
-                backgroundColor: Colors.purple.shade100,
-              ),
-              Chip(
-                label: Text('Abandonei'),
-                backgroundColor: Colors.red.shade100,
-              ),
-            ],
           ),
         ],
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: LivroSelecionado(),
+  ));
 }
